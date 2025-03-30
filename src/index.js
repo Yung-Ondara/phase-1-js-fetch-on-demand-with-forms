fc525b05d306cc00ef62a35cdbf5012171ca77e5
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-30 10:56:09 +0300

    complete fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const init = () => {
-  
-}
+const init = () => {};}
 
 document.addEventListener('DOMContentLoaded', init);
+
+const inputForm = document.querySelector("form");
+inputForm.addEventListener("submit", (event) => {});
+
+const init = () => {
+    const inputForm = document.querySelector("form");
+  
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
+
